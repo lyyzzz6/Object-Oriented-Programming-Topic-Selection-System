2913e97a203c1cc4c66135fec31933e2aebf6c44
lyyzzz6 <mpy1499@163.com>
2023-05-07 19:22:41 +0800

    1

---
+++ cmake-build-debug/CMakeFiles/C__classDesign.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/C++classDesign/Object-Oriented-Programming-Topic-Selection-System/main.cpp" "CMakeFiles/C__classDesign.dir/main.cpp.obj" "gcc" "CMakeFiles/C__classDesign.dir/main.cpp.obj.d"
+  "D:/C++classDesign/Object-Oriented-Programming-Topic-Selection-System/model/Teacher.cpp" "CMakeFiles/C__classDesign.dir/model/Teacher.cpp.obj" "gcc" "CMakeFiles/C__classDesign.dir/model/Teacher.cpp.obj.d"
+  "D:/C++classDesign/Object-Oriented-Programming-Topic-Selection-System/model/Topic.cpp" "CMakeFiles/C__classDesign.dir/model/Topic.cpp.obj" "gcc" "CMakeFiles/C__classDesign.dir/model/Topic.cpp.obj.d"
+  "D:/C++classDesign/Object-Oriented-Programming-Topic-Selection-System/model/student.cpp" "CMakeFiles/C__classDesign.dir/model/student.cpp.obj" "gcc" "CMakeFiles/C__classDesign.dir/model/student.cpp.obj.d"
+  "D:/C++classDesign/Object-Oriented-Programming-Topic-Selection-System/src/menu.cpp" "CMakeFiles/C__classDesign.dir/src/menu.cpp.obj" "gcc" "CMakeFiles/C__classDesign.dir/src/menu.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
